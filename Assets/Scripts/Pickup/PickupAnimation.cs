9c575d3906a919ee101ffa0b9bbf7ed37c209b0a
Michael Kamm <michaeljkamm@gmail.com>
2019-10-05 18:00:31 +0700

    oxygen_tank

--- Assets/Scripts/Pickup/PickupAnimation.cs
+++ Assets/Scripts/Pickup/PickupAnimation.cs
@@ -6,16 +6,21 @@ using UnityEngine;
 public class PickupAnimation : MonoBehaviour
 {
     [SerializeField] private FloatReference pickupRotationSpeed;
+    [SerializeField] private Vector3 rotateAround = Vector3.up;
+
+    [SerializeField] private bool useRandomRotation = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rotateAround = useRandomRotation
+            ? new Vector3(Random.value, Random.value, Random.value).normalized
+            : rotateAround;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.up * pickupRotationSpeed * Time.deltaTime);
+        transform.Rotate(rotateAround * pickupRotationSpeed * Time.deltaTime);
     }
 }
