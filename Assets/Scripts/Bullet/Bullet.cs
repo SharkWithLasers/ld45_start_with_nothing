174a40e833b6e2253bbd07f2b34e6fdfc22d35fd
Michael Kamm <michaeljkamm@gmail.com>
2019-10-05 09:38:19 +0700

    firing bullets

---
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ScriptableObjectArchitecture;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private FloatReference bulletSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * bulletSpeed * Time.deltaTime);
+
+        if (transform.position.x > 5f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player/PlayerFirer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ScriptableObjectArchitecture;
+using UnityEngine;
+
+public class PlayerFirer : MonoBehaviour
+{
+    [SerializeField] private IntReference numBullets;
+
+    [SerializeField] private GameObject bulletPrefab;
+
+    [SerializeField] private FloatReference fireRate;
+
+    [SerializeField] private BoolReference inDebug;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inDebug && Input.GetKeyDown(KeyCode.P))
+        {
+            numBullets.Value += 10;
+        }
+
+        if (Input.GetButtonDown("Fire1") && numBullets > 0)
+        {
+            FireBullet();
+            numBullets.Value -= 1;
+        }
+    }
+
+
+    void FireBullet()
+    {
+        var bulletGO = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+    }
+}
