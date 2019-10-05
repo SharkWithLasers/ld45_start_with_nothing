c244527ac89d711e9bf05a1b4642954f867361b1
Michael Kamm <michaeljkamm@gmail.com>
2019-10-05 23:11:46 +0700

    placeholder commit. part minimap. part planet. some other shit too I think

--- Assets/Scripts/GameLoop/LevelGenerator.cs
+++ Assets/Scripts/GameLoop/LevelGenerator.cs
@@ -1,7 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using ScriptableObjectArchitecture;
 using UnityEngine;
+using Random = UnityEngine.Random;
+
+public enum PickupType
+{
+    OxyTank,
+    Fuel
+}
+
+[Serializable]
+public class PickupItem
+{
+    public PickupType pickupType;
+    public Vector2 pickupPosition;
+}
 
 [CreateAssetMenu(menuName = "SO/GameLoop/LevelGenerator")]
 public class LevelGenerator : ScriptableObject
@@ -18,10 +33,15 @@ public class LevelGenerator : ScriptableObject
 
     [SerializeField] private FloatReference oxytankSeconds;
 
+    [SerializeField] public List<PickupItem> pickupItems; 
+
+    //private Dictionary<>
+
     private float distanceToOxyTank;
 
-    public void Awake()
+    public void OnEnable()
     {
+        pickupItems = new List<PickupItem>();
         //Debug.Log(distanceToOxyTank);
     }
 
@@ -40,7 +60,11 @@ public class LevelGenerator : ScriptableObject
             var nextOxyTankLocation = curPosition + direction * distanceToOxyTank;
 
             Instantiate(oxytankPrefab, nextOxyTankLocation, Quaternion.identity);
-
+            pickupItems.Add(new PickupItem
+            {
+                pickupType = PickupType.OxyTank,
+                pickupPosition = nextOxyTankLocation
+            });
             curPosition = nextOxyTankLocation;
         }
 
