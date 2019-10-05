17c8a8338d8f065e975468bf945cdafd5a247992
Michael Kamm <michaeljkamm@gmail.com>
2019-10-05 21:04:31 +0700

    stopping point. need to make minimap to know good level

---
+++ Assets/Scripts/GameLoop/LevelController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    [SerializeField] private LevelGenerator levelGen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelGen.GenerateLevel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GameLoop/LevelGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ScriptableObjectArchitecture;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/GameLoop/LevelGenerator")]
+public class LevelGenerator : ScriptableObject
+{
+    [SerializeField] private FloatReference assumedVelocity;
+    [SerializeField] private FloatReference driftAcceleration;
+
+    [SerializeField] private FloatReference assumedMinAvgVelocityBetweenOxygenTanks;
+    [SerializeField] private IntReference numOxytankDistanceToGoal;
+
+    [SerializeField] private GameObject planetPrefab;
+    [SerializeField] private GameObject oxytankPrefab;
+    [SerializeField] private GameObject fuelPrefab;
+
+    [SerializeField] private FloatReference oxytankSeconds;
+
+    private float distanceToOxyTank;
+
+    public void Awake()
+    {
+        //Debug.Log(distanceToOxyTank);
+    }
+
+    public void GenerateLevel()
+    {
+        distanceToOxyTank = oxytankSeconds * assumedMinAvgVelocityBetweenOxygenTanks;
+
+
+        // start fuel...
+        var curPosition = new Vector2(10, 0);
+
+        for (var i = 0; i < numOxytankDistanceToGoal.Value; i++)
+        {
+            var direction = new Vector2(Random.value, Random.value).normalized;
+
+            var nextOxyTankLocation = curPosition + direction * distanceToOxyTank;
+
+            Instantiate(oxytankPrefab, nextOxyTankLocation, Quaternion.identity);
+
+            curPosition = nextOxyTankLocation;
+        }
+
+    }
+}
