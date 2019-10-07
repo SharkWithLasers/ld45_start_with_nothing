0c3d88b2d22265a6b0f66a724a7c82938b42fe25
Michael Kamm <michaeljkamm@gmail.com>
2019-10-07 19:41:19 +0700

    escape to quit

--- Assets/Scripts/Application/EscapeToQuit.cs
+++ Assets/Scripts/Application/EscapeToQuit.cs
@@ -13,6 +13,9 @@ public class EscapeToQuit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape) && Application.platform != RuntimePlatform.WebGLPlayer)
+        {
+            Application.Quit();
+        }
     }
 }
